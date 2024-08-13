f6c83ad8a18a9393b17058ee5a4937bbef4af94f
Peter Neuroth <pet.v.ne@gmail.com>
2024-08-13 16:43:17 +0200

    use package in flake
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,21 +1,44 @@
-# file: flake.nix
 {
-  description = "Python application packaged using poetry2nix";
+  description = "Homelab trampoline server plugin for core lightning";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
-
-  outputs = { self, nixpkgs, poetry2nix }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      myPythonApp = mkPoetryApplication { projectDir = ./.; };
-    in
-    {
-      apps.${system}.default = {
-        type = "app";
-        program = "${myPythonApp}/bin/homelab_trampoline_server";
-      };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          homelab-trampoline-server = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.homelab-trampoline-server;
+        };
+
+        # Shell for app dependencies.
+        #
+        #     nix develop
+        #
+        # Use this shell for developing your app.
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.homelab-trampoline-server ];
+        };
+
+        # Shell for poetry.
+        #
+        #     nix develop .#poetry
+        #
+        # Use this shell for changes to pyproject.toml and poetry.lock.
+        devShells.poetry = pkgs.mkShell {
+          packages = [ pkgs.poetry ];
+        };
+      });
 }
